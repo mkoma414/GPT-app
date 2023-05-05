e33fcecbda6592f04a77f902287aab7cfbab03d2
Komatsubara Masato <m-koma@nifty.com>
2023-05-05 12:32:36 +0900

    エラー対応

--- test.py
+++ test.py
@@ -46,9 +46,14 @@ def command_handler(body, say):
 
 # Slackイベントのエンドポイント
 @app.route("/slack/events", methods=["POST"])
-def slack_events():
-  # return handler.handle(request)
-  return
+def slack_events(data, context):
+  if ('challenge' in data):
+    return {
+      "statusCode": 200,
+      "body": json.dumps({'challenge': data['challenge']})
+    }
+
+  return handler.handle(request)
 
 if __name__ == "__main__":
     app.run(debug=True)
